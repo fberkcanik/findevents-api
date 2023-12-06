1e818c236a5519921ee6d92783f38358cbe63ec4
Furkan Berk Canik <furkanberkcanik@outlook.com>
2023-12-06 14:03:35 +0300

    simple event return

--- main.py
+++ main.py
@@ -37,4 +37,59 @@ async def hello():
 """ Returns an array of events """
 @app.get("/events")
 async def get_events():
-    return {"events": ["Event 1", "Event 2"]}
+    """ Structure: Slider Img, Cover Img, Featured, Upcoming, Past, Location, Category, tags, latlng, date, time, price  """
+    return [
+        {
+            "id": 1,
+            "name": "Event 1",
+            "description": "This is the description for Event 1",
+            "sliderImg": "https://picsum.photos/seed/picsum/200/300",
+            "coverImg": "https://picsum.photos/seed/picsum/200/300",
+            "featured": True,
+            "upcoming": True,
+            "past": False,
+            "location": "Location 1",
+            "category": "Category 1",
+            "tags": ["tag1", "tag2", "tag3"],
+            "latlng": [1.0, 2.0],
+            "date": "2021-01-01",
+            "time": "12:00:00",
+            "price": 100.0,
+        },
+        {
+            "id": 2,
+            "name": "Event 2",
+            "description": "This is the description for Event 2",
+            "sliderImg": "https://picsum.photos/seed/picsum/200/300",
+            "coverImg": "https://picsum.photos/seed/picsum/200/300",
+            "featured": False,
+            "upcoming": True,
+            "past": False,
+            "location": "Location 2",
+            "category": "Category 2",
+            "tags": ["tag1", "tag2", "tag3"],
+            "latlng": [1.0, 2.0],
+            "date": "2021-01-01",
+            "time": "12:00:00",
+            "price": 100.0,
+        },
+        {
+            "id": 3,
+            "name": "Event 3",
+            "description": "This is the description for Event 3",
+            "sliderImg": "https://picsum.photos/seed/picsum/200/300",
+            "coverImg": "https://picsum.photos/seed/picsum/200/300",
+            "featured": False,
+            "upcoming": True,
+            "past": False,
+            "location": "Location 3",
+            "category": "Category 3",
+            "tags": ["tag1", "tag2", "tag3"],
+            "latlng": [1.0, 2.0],
+            "date": "2021-01-01",
+            "time": "12:00:00",
+            "price": 100.0,
+        },
+    ]
+
+     
